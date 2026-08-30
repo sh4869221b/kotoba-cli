7eb11a7e1072a4ced23caa4cf27a5ad48d57c8cb
sh4869221b <sh4869221b@gmail.com>
2026-08-30 09:39:55 +0900

    refactor(doctor): remove unused adapter arena

--- src/cli/doctor_cmd.zig
+++ src/cli/doctor_cmd.zig
@@ -4,8 +4,6 @@ const errors = @import("../errors.zig");
 const xdg = @import("../xdg.zig");
 
 pub fn run(original_allocator: std.mem.Allocator, paths: xdg.Paths, cmd_args: []const []const u8) !u8 {
-    var invocation_arena = std.heap.ArenaAllocator.init(original_allocator);
-    defer invocation_arena.deinit();
     var json = false;
     if (cmd_args.len == 2 and std.mem.eql(u8, cmd_args[0], "--format") and std.mem.eql(u8, cmd_args[1], "json")) json = true else if (cmd_args.len != 0) return errors.Error.InvalidArguments;
     return doctor.run(original_allocator, paths, json);
